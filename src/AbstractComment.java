94c46cf9a9fd861a5f7d494d069be58f5be1eb2f
longhuyy <119780080+longhuyy@users.noreply.github.com>
2022-12-04 17:12:02 +0800

    Add files via upload

---
+++ src/AbstractComment.java
@@ -0,0 +1,4 @@
+public abstract class AbstractComment {
+    public abstract MediaComment1 GetMediaComment(String media);//返回接口实现类
+    public abstract TextComment1 GetTextComment(String Text);//返回接口实现类
+}
---
+++ src/Choice.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+
+public class Choice {
+    public static Scanner INPUT = new Scanner(System.in);
+}
